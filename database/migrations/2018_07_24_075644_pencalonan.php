716cb0ee1fe56bc207230807d01e9fd063f90adb
yen <sinyan@jkm.gov.my>
2018-07-24 16:17:41 +0800

    Add migration

--- database/migrations/2018_07_24_075644_pencalonan.php
+++ database/migrations/2018_07_24_075644_pencalonan.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Pingat extends Migration
+class Pencalonan extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,18 @@ class Pingat extends Migration
      */
     public function up()
     {
-        Schema::create('pingat', function (Blueprint $table) {
-		$table -> increments('id');
-		$table -> string('name');
+      Schema::create('pencalonan', function (Blueprint $table) {
+	  $table -> increments('id');
+	  $table -> integer('calon_id');
+	  $table -> integer('sesi_id');
+	  $table -> integer('user_id');
+		$table -> string('asas');
+		$table -> string('ulasan');
 		$table -> timestamps();
 		$table -> softDeletes();
     });
-}
+    }
+
     /**
      * Reverse the migrations.
      *
